258fea8f468190aa73d3f2ab845d2503cefadf3c
alvaro <alvaro@fktech.net>
2023-12-25 04:06:13 -0300

    funcionalidades principales terminadas

---
+++ js/TicTacToe.js
@@ -0,0 +1,164 @@
+/* **********************************************************************************************
+ * Alvaro Valencia Rojas - TicTacToe
+ * 12/2023
+ *
+ * Thing to do
+ * - Make a validator for the initial values
+ * - Only one tipe choice on array game
+ * - Clean all square when a player wins the game
+ *************************************************************************************************/
+let square1 = document.getElementById("square1");
+let square2 = document.getElementById("square2");
+let square3 = document.getElementById("square3");
+let square4 = document.getElementById("square4");
+let square5 = document.getElementById("square5");
+let square6 = document.getElementById("square6");
+let square7 = document.getElementById("square7");
+let square8 = document.getElementById("square8");
+let square9 = document.getElementById("square9");
+
+let time = document.getElementById("time");
+let turn = document.getElementById("turn");
+let score = document.getElementById("score");
+
+let turnBoolean;
+let flagTurn = true;
+let X = "X";
+let O = "O";
+
+let strike = false;
+let allSquare = [];
+
+let player1 = {
+  name: "",
+  type: "",
+};
+let player2 = {
+  name: "",
+  type: "",
+};
+
+let game = [[], [], [], [], [], [], [], [], []];
+let winner;
+
+/* Initial Values*/
+
+let initial = () => {
+  player1.name = prompt("Ingrese el nombre del Jugador 1");
+  player2.name = prompt("Ingrese el nombre del Jugador 2");
+
+  player1.type = prompt("Jugador 1: ¿Que elegís para jugar X/O?");
+  player2.type = prompt("Jugador 2: ¿Que elegís para jugar X/O?");
+};
+
+//initial();
+
+/* game */
+
+window.addEventListener("click", (e) => {
+  let square = e.target;
+  if (square.className == "square") {
+    switch (square.id) {
+      case "square1":
+        choose(0);
+        break;
+      case "square2":
+        choose(1);
+        break;
+      case "square3":
+        choose(2);
+        break;
+      case "square4":
+        choose(3);
+        break;
+      case "square5":
+        choose(4);
+        break;
+      case "square6":
+        choose(5);
+        break;
+      case "square7":
+        choose(6);
+        break;
+      case "square8":
+        choose(7);
+        break;
+      case "square9":
+        choose(8);
+        break;
+
+      default:
+        break;
+    }
+    logical();
+    putImageInside(square);
+    endGame();
+    console.log("---game", game[2]);
+  }
+});
+
+let putImageInside = (square) => {
+  let img = document.createElement("img");
+  img.className = "img";
+  flagTurn ? (img.src = "../img/cruz.svg") : (img.src = "../img/circulo.svg");
+  square.append(img);
+};
+
+let choose = (pos) => {
+  if (flagTurn) {
+    /* turnBoolean = false; */
+    flagTurn = false;
+    turn.innerText = X;
+  } else {
+    /* turnBoolean = true; */
+    flagTurn = true;
+    turn.textContent = O;
+  }
+  game[pos] = turn.innerText
+};
+
+/* game logica */
+function logical() {
+  /* horizontal line  */
+  if (game[0] == game[1] && game[0] == game[2]) {
+    strike = true;
+  }
+  if (game[3] == game[4] && game[3] == game[5]) {
+    strike = true;
+  }
+  if (game[6] == game[7] && game[6] == game[8]) {
+    strike = true;
+  }
+  /* vertical line  */
+  if (game[0] == game[3] && game[0] == game[6]) {
+    strike = true;
+  }
+  if (game[1] == game[4] && game[1] == game[7]) {
+    strike = true;
+  }
+  if (game[2] == game[5] && game[2] == game[8]) {
+    strike = true;
+  }
+  /* diagonal */
+  if (game[0] == game[4] && game[0] == game[8]) {
+    strike = true;
+  }
+  if (game[2] == game[4] && game[2] == game[6]) {
+    strike = true;
+  }
+}
+
+/* end game */
+
+function endGame() {
+  if (strike) {
+    strike = false;
+    game = [[], [], [], [], [], [], [], [], []];
+
+    allSquare = document.querySelectorAll("img");
+
+    allSquare.forEach((element) => {
+      element.remove();
+    });
+  }
+}
